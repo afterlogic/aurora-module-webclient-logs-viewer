1338b7f99bba72398a9445f670315df22353ef25
sash04ek <a.ovcharov@gmail.com>
2017-03-03 12:40:50 +0300

    global api refactoring added namespaces

--- module.php
+++ module.php
@@ -2,7 +2,7 @@
 
 namespace Aurora\Modules;
 
-class LogsViewerWebclientModule extends \Aurora\System\AbstractModule
+class LogsViewerWebclientModule extends \Aurora\System\Module\AbstractModule
 {
 	public function init() 
 	{
