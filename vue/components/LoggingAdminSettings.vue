c58d25fef2bc049264536508512ddfffac6dd16a
Vasil Sokolov <vasil@afterlogic.com>
2023-04-02 14:35:31 +0200

    Switched to Quasar2/VueJs3

--- vue/components/LoggingAdminSettings.vue
+++ vue/components/LoggingAdminSettings.vue
@@ -134,7 +134,7 @@ export default {
   },
 
   beforeRouteLeave (to, from, next) {
-    this.doBeforeRouteLeave(to, from, next)
+    this.$root.doBeforeRouteLeave(to, from, next)
   },
 
   computed: {
